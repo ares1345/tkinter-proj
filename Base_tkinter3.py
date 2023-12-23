5342291b516f5faeced49816efdd00950a02b549
ares1345 <ares1345istaken@gmail.com>
2023-12-23 17:06:08 +0100

    More changes

--- Base_tkinter3.py
+++ Base_tkinter3.py
@@ -2,8 +2,22 @@ import tkinter as tk
 # from tkinter import ttk
 import ttkbootstrap as ttk
 
+def button_func():
+    print("t")
+
 window = ttk.Window(themename = 'darkly') 
-window.title("Getting and setting widgets")
+window.title("Getting and setting widgetsgi")
 window.geometry("800x500") #widthxheight
+
+label = ttk.Label(master=window, text='text')
+label.pack()
+
+entry = ttk.Button(master=window)
+entry.pack()
+
+button = ttk.Button(master=window, text ="buttton", command=button_func)
+button.pack()
+
+
 
 window.mainloop()
