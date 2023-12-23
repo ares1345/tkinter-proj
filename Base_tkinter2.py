e260636448125f3de3384452c9a7075af1afa5c1
ares1345 <ares1345istaken@gmail.com>
2023-12-23 16:49:26 +0100

    Some changes

--- Base_tkinter2.py
+++ Base_tkinter2.py
@@ -8,13 +8,17 @@ window = ttk.Window(themename = 'darkly')
 window.title("Editeur de texte")
 window.geometry("800x500") #widthxheight
 
+#ttk widgets
+label = ttk.Label(master = window, text = "bruh")
+label.pack()
+
+
+
 # tkwidgets
 textbox = tk.Text(master = window) #we put it inside window
 textbox.pack(pady = 15)
 
-#ttk widgets
-label = ttk.Label(master = window, text = "bruh")
-label.pack()
+
 
 window.mainloop() 
 #updates the gui and checks for movements, make sure it is at the end of the file 
