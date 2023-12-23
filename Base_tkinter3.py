643db83f20c71c3d31285979bceb3cf45ad656c1
ares1345 <ares1345istaken@gmail.com>
2023-12-23 17:03:25 +0100

    Changes

---
+++ Base_tkinter3.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+# from tkinter import ttk
+import ttkbootstrap as ttk
+
+window = ttk.Window(themename = 'darkly') 
+window.title("Getting and setting widgets")
+window.geometry("800x500") #widthxheight
+
+window.mainloop()
