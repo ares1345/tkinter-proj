8122a7ebdd8bd2c0bb696a94146c31e7403eeea7
ares1345 <ares1345istaken@gmail.com>
2023-12-23 16:59:47 +0100

    changes

--- Base_tkinter2.py
+++ Base_tkinter2.py
@@ -8,16 +8,33 @@ window = ttk.Window(themename = 'darkly')
 window.title("Editeur de texte")
 window.geometry("800x500") #widthxheight
 
+def button_func():
+    print('test')
+
+
+
 #ttk widgets
 label = ttk.Label(master = window, text = "bruh")
 label.pack()
-
-
 
 # tkwidgets
 textbox = tk.Text(master = window) #we put it inside window
 textbox.pack(pady = 15)
 
+#tk-entry
+entry = ttk.Entry(master = window)
+entry.pack()
+
+label2 = ttk.Label(master=window, text="My label")
+label2.pack()
+button2 = ttk.Button(master=window, command=lambda: print("hello") #lambda functions are undeclared functions
+button2.pack()
+
+
+
+#tk-button
+button = ttk.Button(master=window, text= 'A button', command=button_func)) 
+button.pack(pady = 10) 
 
 
 window.mainloop() 
