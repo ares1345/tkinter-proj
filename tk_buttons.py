d15466d8d1441d5ad0afdc5c9896ccba72a1f853
ares1345 <ares1345istaken@gmail.com>
2023-12-25 21:57:20 +0100

    New file

---
+++ tk_buttons.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+# from tkinter import ttk
+import ttkbootstrap as ttk
+
+window = ttk.Window(themename = 'darkly') 
+window.title("tkinter variables")
+window.geometry("800x500") #widthxheight
+
+
+window.mainloop()
