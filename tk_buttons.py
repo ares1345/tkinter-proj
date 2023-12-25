90f3fd4e9f120c21f15e6bcac76f587aec59b2bc
ares1345 <ares1345istaken@gmail.com>
2023-12-25 21:59:50 +0100

    Changes

--- tk_buttons.py
+++ tk_buttons.py
@@ -6,5 +6,15 @@ window = ttk.Window(themename = 'darkly')
 window.title("tkinter variables")
 window.geometry("800x500") #widthxheight
 
+button = ttk.Button(window, text="button")
+#it is not necessar to write master =, the 1st argument is alr assumed to be the master variable
+
+
+
+
+
+
+
+
 
 window.mainloop()
