a6dba612877742a2e63c1de44ca24fe86d0191d3
ares1345 <ares1345istaken@gmail.com>
2023-12-25 21:25:26 +0100

    Further changes

--- Base_tkinter3.py
+++ Base_tkinter3.py
@@ -5,6 +5,9 @@ import ttkbootstrap as ttk
 def button_func():
     print("t")
 
+    #Update label
+    label.config(text='some other text') #modify a setting of said variable inside the widget
+
 window = ttk.Window(themename = 'darkly') 
 window.title("Getting and setting widgetsgi")
 window.geometry("800x500") #widthxheight
@@ -13,6 +16,7 @@ label = ttk.Label(master=window, text='text')
 label.pack()
 
 entry = ttk.Button(master=window)
+output_str = tk.StringVar()
 entry.pack()
 
 button = ttk.Button(master=window, text ="buttton", command=button_func)
