78e1b2e0fb5d2ae8982f275ee491e93b8c8d40db
ares1345 <ares1345istaken@gmail.com>
2023-12-25 21:46:05 +0100

    Changes

--- tkinter_var.py
+++ tkinter_var.py
@@ -6,10 +6,15 @@ window = ttk.Window(themename = 'darkly')
 window.title("tkinter variables")
 window.geometry("800x500") #widthxheight
 
-label = ttk.Label(master=window, text="test")
+string_var = tk.StringVar(value="test") #or IntVar, BooleanVar, DoubleVar
+#value sets the default value of said variable
+
+
+
+label = ttk.Label(master=window, text="label", textvariable=string_var)
 label.pack()
 
-entry = ttk.Entry(master=window)
+entry = ttk.Entry(master=window, textvariable=string_var)
 entry.pack()
 
 
