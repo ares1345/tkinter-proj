bb3eaed78fa7119603d01e394f2a7e2d15f92515
ares1345 <ares1345istaken@gmail.com>
2023-12-25 21:51:36 +0100

    Changes

--- tkinter_var.py
+++ tkinter_var.py
@@ -6,8 +6,13 @@ window = ttk.Window(themename = 'darkly')
 window.title("tkinter variables")
 window.geometry("800x500") #widthxheight
 
-string_var = tk.StringVar(value="test") #or IntVar, BooleanVar, DoubleVar
-#value sets the default value of said variable
+def button_func():
+    print(string_var.get())
+    string_var.set('button pressed')
+
+
+string_var = tk.StringVar() #or IntVar, BooleanVar, DoubleVar
+#value = "txt" sets the default value of said variable, in the ()
 
 
 
@@ -16,6 +21,9 @@ label.pack()
 
 entry = ttk.Entry(master=window, textvariable=string_var)
 entry.pack()
+
+button = ttk.Button(master=window, text="button", command=button_func)
+button.pack()
 
 
 
