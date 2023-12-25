9d5b2f36cc07121d310976b97900d2ae656cc19d
ares1345 <ares1345istaken@gmail.com>
2023-12-25 22:06:21 +0100

    changes

--- tk_buttons.py
+++ tk_buttons.py
@@ -6,8 +6,17 @@ window = ttk.Window(themename = 'darkly')
 window.title("tkinter variables")
 window.geometry("800x500") #widthxheight
 
-button = ttk.Button(window, text="button")
+def button_func():
+    print("t")
+
+
+button_str = tk.StringVar()
+
+
+button = ttk.Button(window, text="button", command=button_func, textvariable=button_str)
 #it is not necessar to write master =, the 1st argument is alr assumed to be the master variable
+#reminder that textvariable overwrites text
+button.pack()
 
 
 
